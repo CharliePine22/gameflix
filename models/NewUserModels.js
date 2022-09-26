0cd6b9e99870b199b6eddf190b0e0c0b0bc9df66
Charles Pine <charlespine22@gmail.com>
2022-09-26 12:52:22 -0500

    Moved server dir contents into root folder

---
+++ models/NewUserModels.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const newUserTemplate = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+    default: 'black',
+  },
+  avatar: {
+    type: String,
+  },
+  profiles: {
+    type: Array,
+    default: [],
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('users', newUserTemplate);

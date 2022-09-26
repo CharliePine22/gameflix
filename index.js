de1a2d35e3f4ea7f4495466cd3c96a64c10163f6
Charles Pine <charlespine22@gmail.com>
2022-09-26 13:42:15 -0500

    Adjusted CORS options

--- index.js
+++ index.js
@@ -13,18 +13,11 @@ mongoose.connect(process.env.MONGODB_ACCESS, () => {
 });
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+app.options('*', cors());
 app.use('/app', routesUrls);
 app.use('/uploads', express.static('uploads'));
 app.use(express.static(path.resolve(__dirname, './client/build')));
-app.use(function (request, response, next) {
-  response.header('Access-Control-Allow-Origin', '*');
-  response.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
 app.listen(process.env.PORT || 5000, () =>
   console.log('Server is up and running!')
 );

ebf0236bd25373b2e3d3a1caee563960d1396719
Charles Pine <charlespine22@gmail.com>
2022-09-26 16:30:07 -0500

    Removed '/' from env BASE_URL

--- index.js
+++ index.js
@@ -25,13 +25,13 @@ app.use(cors(corsOptions));
 app.use('/app', routesUrls);
 app.use('/uploads', express.static('uploads'));
 
-// if (process.env.PORT) {
-//   // Set static folder
-//   app.use(express.static('client/build'));
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   });
-// }
+if (process.env.PORT) {
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(process.env.PORT || 5000, () =>
   console.log('Server is up and running!')

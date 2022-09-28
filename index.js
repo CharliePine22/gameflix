f22540e2489eacd39f3f612381820c8a332562f8
Charles Pine <charlespine22@gmail.com>
2022-09-28 16:36:32 -0500

    Changed backend localhost from 5000 to 30001

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const routesUrls = require('./routes/routes');
-const cors = require('cors');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const routesUrls = require("./routes/routes");
+const cors = require("cors");
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGODB_URI, () => {
-  console.log('Database Connected!');
+  console.log("Database Connected!");
 });
 
 app.use(express.json());
 
 const corsOptions = {
-  origin: ['http://localhost:3000', 'https://gameflixx.netlify.app/'],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  origin: ["http://localhost:3000", "https://gameflixx.netlify.app/"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
-app.use('/app', routesUrls);
-app.use('/uploads', express.static('uploads'));
+app.use("/app", routesUrls);
+app.use("/uploads", express.static("uploads"));
 
 if (process.env.PORT) {
   // Set static folder
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
-app.listen(process.env.PORT || 5000, () =>
-  console.log('Server is up and running!')
+app.listen(process.env.PORT || 3001, () =>
+  console.log("Server is up and running!")
 );
